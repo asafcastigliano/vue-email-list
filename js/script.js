83f542680a0959e11491fa8b2b89a6fdc98cf2e5
asafcastigliano <asafcastigliano@gmail.com>
2023-12-20 16:45:48 +0100

    esercizio finito

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            emails: []
+        }
+    },
+
+    created(){
+        for(let i = 0; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(response => {
+                this.emails.push(response.data.response);
+            });
+        }
+    },
+
+    methods: {
+    }
+}).mount('#app');
